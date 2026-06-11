400f54c0cab4077008538eb5e3e5bb22d2991728
ahmaddaniaal <ahmaddaniaal30@gmail.com>
2026-06-11 15:12:19 +0700

    feat: implement user registration API and DB schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "mysql://root:root@127.0.0.1:3306/belajar_vibe_coding",
+    url: process.env.DATABASE_URL || "mysql://root:@127.0.0.1:3306/belajar_vibe_coding",
   },
 });
